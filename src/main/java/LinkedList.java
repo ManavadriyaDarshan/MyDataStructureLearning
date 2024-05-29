17721063c9f034715a8e64dcf6497ef69f97b954
Darshan Manavadariya <manavadariyadarshan@gmail.com>
2024-05-29 17:41:35 +0530

    Ready basic structure of linkedlist

---
+++ src/main/java/LinkedList.java
@@ -0,0 +1,16 @@
+public class LinkedList {
+    static Node head;
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
